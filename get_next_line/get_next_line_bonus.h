86272796671c2d48a5e987a729a875bd7db647ef
Ömer Faruk <151129176+omrfrkzu@users.noreply.github.com>
2024-10-03 11:09:18 +0300

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef MAX_FILES_OPENED
+#  define MAX_FILES_OPENED 65536
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
